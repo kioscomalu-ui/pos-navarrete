a052983ef820145925f6d73d7f9f61c94f3092af
Daniel <ieupsocios@gmail.com>
2026-08-19 16:49:14 -0300

    fix: reconectar articulos

--- apps/web/app/(app)/articulos/[id]/page.tsx
+++ apps/web/app/(app)/articulos/[id]/page.tsx
@@ -3,6 +3,8 @@ import { getSesion, puedeEditarCatalogo } from '@/lib/sesion';
 import { redirect, notFound } from 'next/navigation';
 import { FormArticulo } from '@/components/articulos/FormArticulo';
 
+export const dynamic = 'force-dynamic';
+
 export default async function EditarArticulo({
   params,
 }: {
